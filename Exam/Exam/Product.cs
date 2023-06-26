e10d8943acc811004da8c467608d233eb382f454
Cylin04 <113584982+Cylin04@users.noreply.github.com>
2023-06-26 15:13:12 +0700

    Add files via upload

---
+++ Exam/Exam/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam
+{
+        abstract class Product
+        {
+            private string productId;
+            private string name;
+            private double price;
+            private string producer;
+             
+            public Product(string productId, string name, double price, string producer)
+            {
+                this.productId = productId;
+                this.name = name;
+                this.price = price;
+                this.producer = producer;
+            }
+
+            public abstract double ComputeTax();
+        }
+
+    }
+
